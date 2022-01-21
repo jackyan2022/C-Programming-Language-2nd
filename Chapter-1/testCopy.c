577bb788ff9dcd681c755f606161dc9d2dfd720b
yan zhong <jackyan63@gmail.com>
2022-01-21 13:25:41 -0500

    print EOF and getchar() != EOF

--- Chapter-1/testCopy.c
+++ Chapter-1/testCopy.c
@@ -3,8 +3,11 @@
 int main() {
     int c;
     c = getchar();
+    printf("EOF is %d\n", EOF);
     while(c != EOF) {
         putchar(c);
         c = getchar();
+        printf(" getchar is EOF is %d\n", getchar() != EOF);
     }
+    return 0;
 }
