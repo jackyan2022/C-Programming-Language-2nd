64a6795cb35aedc87badfb2596f5f7560823c5a9
yan zhong <jackyan63@gmail.com>
2022-02-02 20:32:49 -0500

    dont know how to do

---
+++ Chapter-5/testFind-x-nPattern.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+#define MAXLINE 1000
+
+int getLine(char *line, int max);
+
+int main(int argc, char *argv[]) {
+    char line[MAXLINE];
+    int lineno = 0;
+    int c, except = 0, number = 0, found = 0;
+    while(--argc > 0 && (*++argv)[0] == '-')
+        while(c = *++argv[0])
+            switch(c) {
+                case 'x':
+                    except = 1;
+                    break;
+                case 'n':
+                    number = 1;
+                    break;
+                default:
+                    printf("find: illegal option %c\n", c);
+                    argc = 0;
+                    found = -1;
+                    break;
+            }
+    if (argc != 1)  
+        printf("Usage: find -x -n pattern\n");
+    else 
+        while (getLine(line, MAXLINE) > 0) {
+            lineno++;
+            if((strstr(line, *argv) != NULL) != except) {
+                if (number)
+                    printf("%1d:", lineno);
+                printf("%s", line);
+                found++;
+            }
+        }
+    return found;
+}
+
+int getLine(char s[], int lim) {
+    int c, i;
+    i = 0;
+    while (--lim > 0 && (c=getchar()) != EOF && c != '\n')
+        s[i++] = c;
+    if (c == '\n')
+        s[i++] = c;
+    s[i] = '\0';
+    return i;
+}
