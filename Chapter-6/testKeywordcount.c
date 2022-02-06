2958d9fdd780f125337cf6ccc1b13cb912ce14a6
yan zhong <jackyan63@gmail.com>
2022-02-06 17:34:02 -0500

    test keyword count

--- Chapter-6/testKeywordcount.c
+++ Chapter-6/testKeywordcount.c
@@ -36,3 +36,59 @@ int main() {
             printf("%4d %s\n", keytab[n].count, keytab[n].word);
     return 0;
 }
+
+int binsearch(char *word, struct key tab[], int n) {
+    int cond;
+    int low, high, mid;
+
+    low = 0;
+    high = n - 1;
+    while (low <= high) {
+        mid = (low + high) / 2;
+        if ((cond = strcmp(word, tab[mid].word)) < 0)
+                high = mid - 1;
+        else if (cond > 0) 
+                low = mid + 1;
+        else 
+                return mid;
+    }
+    return -1;
+}
+
+int getword(char *word, int lim) {
+    int c, getch(void);
+    void ungetch(int);
+    char *w = word;
+
+    while (isspace(c = getch()))
+        ;
+    if(c != EOF)
+        *w++ = c;
+    if(!isalpha(c)) {
+        *w = '\0';
+        return c;
+    }
+    for ( ; --lim > 0; w++) 
+        if(!isalnum(*w = getch())) {
+            ungetch(*w);
+            break;
+        }
+    *w = '\0';
+    return word[0];
+}
+
+#define BUFSIZE 100
+
+char buf[BUFSIZE];
+int bufp = 0;
+
+int getch(void) {
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c) {
+    if(bufp >= BUFSIZE)
+        printf("ungetch: toow many characters\n");
+    else 
+        buf[bufp++] = c;
+}
